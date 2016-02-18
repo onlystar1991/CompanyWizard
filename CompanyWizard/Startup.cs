bd526e58851a399f3178dac4d0e3fcb10b05f537
onlystar1991 <onlystar1991@hotmail.com>
2016-02-18 01:40:45 +0100

    Initial Commit

---
+++ CompanyWizard/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CompanyWizard.Startup))]
+namespace CompanyWizard
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
